8695b7c4d9c88eec5f991cad2eeb76183bba7c44
starr-5 <studying12567@gmail.com>
2026-05-30 07:08:03 +0000

    task1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function for concatenation.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes two string arguments and returns a concatenated string.
+    """
+    return str1 + str2
