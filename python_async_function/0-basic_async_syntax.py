de57c774aa27014599b3b33f9a97a3536967b1b0
starr-5 <studying12567@gmail.com>
2026-05-30 05:50:21 +0000

    Create async coroutine wait_random

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+"""Basic async syntax module."""
 
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a random delay and returns it."""
+    """Wait for a random delay and return it."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
