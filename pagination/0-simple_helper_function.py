da4e368deb2a36a0ac9ddb98c43ebd5a5bc5e4cd
starr-5 <studying12567@gmail.com>
2026-06-14 09:33:01 +0000

    Add index_range function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def index_range(page: int, page_size: int) -> tuple:
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
