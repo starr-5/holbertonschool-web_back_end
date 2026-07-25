95f9eff9dc2db6f0eccbecaef365de85d9025f2a
starr-5 <studying12567@gmail.com>
2026-07-25 13:40:29 +0000

    task8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-    if (startString === undefined || startString === '') {
+    if (typeof startString !== 'string' || startString === '') {
       return '';
     }
   
