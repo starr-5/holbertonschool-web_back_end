8f47cce4d0839db1c6254ad66b3e6674239afb81
starr-5 <studying12567@gmail.com>
2026-07-26 09:40:50 +0000

    task2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -10,7 +10,6 @@ function countStudents(path) {
   }
 
   const lines = data.split('\n').filter((line) => line.trim() !== '');
-
   const students = lines.slice(1);
 
   console.log(`Number of students: ${students.length}`);
@@ -29,11 +28,11 @@ function countStudents(path) {
     fields[field].push(firstname);
   }
 
-  for (const field in fields) {
+  Object.keys(fields).forEach((field) => {
     console.log(
-      `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+      `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
     );
-  }
+  });
 }
 
 module.exports = countStudents;
