96092f1c093580acb7b9095f9ec978f85c8d49cb
starr-5 <studying12567@gmail.com>
2026-07-26 09:08:34 +0000

    task1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.setEncoding('utf8');
+
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
