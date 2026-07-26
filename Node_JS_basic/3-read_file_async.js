8def475229b373e68ca1899deae21e66ce6c6879
starr-5 <studying12567@gmail.com>
2026-07-26 09:44:02 +0000

    task

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {};
+
+      for (const student of students) {
+        const values = student.split(',');
+        const firstname = values[0];
+        const field = values[3];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstname);
+      }
+
+      Object.keys(fields).forEach((field) => {
+        console.log(
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+        );
+      });
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
